622bf0fb039d7af8c3e8c248ff9691653d307137
Soyalkhan <soyalkhan76@gmail.com>
2025-05-08 18:35:47 +0530

    style: format userSchema definition for improved readability

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String },
-  address: { type: String },
-  gender: { type: String, enum: ["Male", "Female", "Other"] },
-  password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  otp: { type: String },
-  otpExpires: { type: Date },
-  role: { type: String, enum: ["user", "admin"], default: "user" }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String },
+    address: { type: String },
+    gender: { type: String, enum: ["Male", "Female", "Other"] },
+    password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    otp: { type: String },
+    otpExpires: { type: Date },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
